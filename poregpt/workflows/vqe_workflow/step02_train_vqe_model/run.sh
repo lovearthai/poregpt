e61186d6eeeb3b6a772a649879879311a5920481
jiaoshuai <jiaoshuaihit@gmail.com>
2026-01-26 19:20:36 +0800

    xiu

--- poregpt/workflows/vqe_workflow/step02_train_vqe_model/run.sh
+++ poregpt/workflows/vqe_workflow/step02_train_vqe_model/run.sh
@@ -1,5 +1,3 @@
 #export CUDA_VISIBLE_DEVICES=1,2
 export WANDB_API_KEY=748830e9b9acdf804bb0baad0eb82e6ca2592354
-torchrun --nproc_per_node=4 --master_port 29501 -m poregpt.tokenizers.vqe_tokenizer.vqe_train --config config_c16k_scratch.yaml
-#python3  -m accelerate.launch  -m poregpt.tokenizers.vqe_tokenizer.vqe_train --config config_pass21_c64k_ema.yaml 
-#accelerate launch /mnt/nas_syy/default/poregpt/poregpt/poregpt/tokenizers/vqe_tokenizer/vqe_train.py  --config config_pass21_c64k_ema.yaml 
+torchrun --nproc_per_node=4 --master_port 29501 -m poregpt.tokenizers.vqe_tokenizer.vqe_train --config config_c64k_scratch.yaml
